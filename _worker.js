3ef67f941ee53d8aae6eb44a8097ba87bc8f3e8a
1259735162 <116120816+1259735162@users.noreply.github.com>
2025-06-02 17:15:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="hax.kksxsyj.cf";
+        url.hostname="www.ltyfuq.asia:1025";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
