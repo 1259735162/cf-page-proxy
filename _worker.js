16f0520c739e6feae3a2c194eb7113998552eb6a
1259735162 <116120816+1259735162@users.noreply.github.com>
2025-06-03 23:52:42 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="www.ltyfuq.asia:1025";
+        url.hostname="boke.ltyfuq.asia";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
